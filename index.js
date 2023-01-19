e6520e4b35ea9c8eea0547f44b6ae661c6d9e9bb
Krishna <99634559+Anand-Krishna-555@users.noreply.github.com>
2023-01-19 17:36:14 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// module.exports = function(){
-//     import('./foo.js').then( ({default:foo}) => console.log(foo) )
-// }
-
-window.onload = (e) => {
-    console.log("Hello from Bundler Practice");
-};
