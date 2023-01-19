67df450c8e9e82d45fce8f8a7cd3727c18e6250b
Krishna <99634559+Anand-Krishna-555@users.noreply.github.com>
2023-01-19 16:28:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// module.exports = function(){
+//     import('./foo.js').then( ({default:foo}) => console.log(foo) )
+// }
+
+window.onload = (e) => {
+    console.log("Hello from Bundler Practice");
+};
